e38ca5a2b498302ca60b0de03fac125a6e1e0067
liangchen08330112 <3366211746@qq.com>
2022-05-20 19:44:18 +0800

    2022.05.20 19:44梁宸提交。

--- app/src/main/java/com/example/bighomework_v20/Users/GradeImportActivity.java
+++ app/src/main/java/com/example/bighomework_v20/Users/GradeImportActivity.java
@@ -11,23 +11,32 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bighomework_v20.R;
 
 public class GradeImportActivity extends AppCompatActivity implements View.OnClickListener {
-    private MyHelper helper;
+
     private EditText editText_name,editText_grades;
     private Button button_insert,button_delete,button_update,button_retrieve;
+    private TextView textView_show;
+
+    MyHelper helper;
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_import);
         initView();
+
+        helper = new MyHelper(this,"grades.db",null,1);
+        database = helper.getWritableDatabase();
     }
 
     private void initView() {
@@ -37,6 +46,7 @@ public class GradeImportActivity extends AppCompatActivity implements View.OnCli
         button_delete = findViewById(R.id.button_delete);
         button_update = findViewById(R.id.button_update);
         button_retrieve = findViewById(R.id.button_retrieve);
+        textView_show = findViewById(R.id.textView_show);
 
         button_insert.setOnClickListener(this);
         button_delete.setOnClickListener(this);
@@ -46,94 +56,90 @@ public class GradeImportActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onClick(View v) {
-        String name,grades;
-        SQLiteDatabase database;
-        ContentValues values;
-        switch (v.getId()){
-            case R.id.button_insert:
-                name = editText_name.getText().toString().trim();
-                grades = editText_grades.getText().toString().trim();
-                database = helper.getWritableDatabase();
-                values = new ContentValues();
-                values.put("name",name);
-                values.put("grades",grades);
-                database.insert("info",null,values);
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("提示").setIcon(R.drawable.ic_zhengque).setMessage("录入成功");
-                builder.setCancelable(true);
-                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-                //关闭数据库
-                database.close();
-                break;
-            case R.id.button_delete:
-
-                break;
-            case R.id.button_update:
-                database = helper.getWritableDatabase();
-                values = new ContentValues();
-                values.put("grades",grades = editText_grades.getText().toString().trim());
-                database.update("info",values,"name=?",new String[]{editText_name.getText().toString().trim()});
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-                builder1.setTitle("提示").setIcon(R.drawable.ic_zhengque).setMessage("成绩修改成功");
-                builder1.setCancelable(true);
-                builder1.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-                AlertDialog dialog1 = builder1.create();
-                dialog1.show();
-                break;
-            case R.id.button_retrieve:
-                //获得可读权限
-                database = helper.getReadableDatabase();
-                //Cursor集游标，查询数据
-                Cursor cursor = database.query("info",null,null,null,null,null,null);
-                //如果未获取数据
-                if(cursor.getCount()==0){
-                    AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
-                    builder2.setIcon(R.drawable.ic_jinggao).setTitle("提示").setMessage("暂无数据！");
-                    builder2.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+        String name = editText_name.getText().toString().trim();
+        if(TextUtils.isEmpty(name)){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("提示").setMessage("请输入学生姓名").setIcon(R.drawable.ic_alert).setCancelable(true)
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            dialogInterface.dismiss();
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
                         }
                     });
-                    builder2.setCancelable(true);
-                    AlertDialog dialog2 = builder2.create();
-                    dialog2.show();
-                }else{ //获取到数据后从第一个数据开始显示文本
-                    cursor.moveToFirst();
-//                    textView_show.setText("姓名："+cursor.getString(1)+"，成绩："+cursor.getString(2));
-                }  //往下读一行
-                while (cursor.moveToNext()) {
-//                    textView_show.append("\n"+"姓名："+cursor.getString(1)+"，成绩："+cursor.getString(2));
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            return;
+        }
+        String grades = editText_grades.getText().toString().trim();
+        switch (v.getId()){
+            case R.id.button_insert:
+                if(TextUtils.isEmpty(grades)){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("提示").setMessage("请输入学生成绩").setIcon(R.drawable.ic_alert).setCancelable(true)
+                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return;
                 }
-                Toast.makeText(this,"成绩查询完毕",Toast.LENGTH_SHORT).show();
-                //关闭集游标
-                cursor.close();
-                //关闭数据库
-                database.close();
+                ContentValues values = new ContentValues();
+                values.put("name",name);
+                values.put("grades",grades);
+                long insertID = database.insert("info",null,values);
+                if(insertID==-1){
+                    Toast.makeText(this,"成绩录入成功",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this,"成绩录入失败",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.button_delete:
+                int deleteLines = database.delete("info","name=?",new String[]{name});
+                Toast.makeText(this,"成功删除第"+deleteLines+"条记录",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_update:
+                if(TextUtils.isEmpty(grades)){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("提示").setMessage("请输入学生成绩").setIcon(R.drawable.ic_alert).setCancelable(true)
+                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return;
+                }
+                ContentValues values2 = new ContentValues();
+                values2.put("grades",grades);
+                int updateLines = database.update("info",values2,"name=?",new String[]{name});
+                Toast.makeText(this,"成功修改"+updateLines+"条记录",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_retrieve:
+                Cursor cursor = database.rawQuery("select * from info",null);
+                while (cursor.moveToNext()){
+                    textView_show.setText("姓名："+cursor.getString(0)+"，成绩："+cursor.getString(1));
+                }
+                break;
+            default:
                 break;
         }
+
     }
 
-    private class MyHelper extends SQLiteOpenHelper {
+    class MyHelper extends SQLiteOpenHelper{
+
         public MyHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-            super(context, "grades.db", null, 1);
+            super(context, name, factory, version);
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-
+            db.execSQL("create table if not exists info(name varchar(20),grades varchar(10),primary key(name))");
         }
 
         @Override
