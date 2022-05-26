49bd85dbe9feb31c6f61ea67a546e451c93067a3
William-KeLi <2491282230@qq.com>
2022-05-26 13:25:49 +0800

    1

--- app/src/main/java/com/example/bighomework_v20/Dao/MySQLiteHelper.java
+++ app/src/main/java/com/example/bighomework_v20/Dao/MySQLiteHelper.java
@@ -16,7 +16,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "create table if not exists user_info(user_name vachar,user_password)";
+        String sql = "create table if not exists user_info(user_name varchar(50),user_password varchar(50))";
         db.execSQL(sql);
 
     }
